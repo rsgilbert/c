332dea3dcccffffc360dcbb2fd774d9681cb2d51
ssg <ssg@hrpsolutions.com>
2022-02-25 20:54:21 +0000

    Did exercise of printing one word per line

---
+++ one-word-per-line.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    while( (c = getchar()) != EOF) {
+        if(c == ' ' || c == '\t') {
+            c = '\n';
+        }
+        putchar(c);
+    }
+}
